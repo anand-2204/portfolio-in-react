7ffc70a3f4584e10881d446cdc55b0c4bbcb487b
anand kumar <anandkumar22042000@gmail.com>
2026-01-16 18:40:13 +0530

    hero section updated

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './Hero.css';
-
+import {Link} from 'react-router-dom'
 function Hero() {
   return (
     <section id="home" className="hero">
@@ -11,8 +11,8 @@ function Hero() {
           I build amazing web applications with React and modern technologies
         </p>
         <div className="hero-buttons">
-          <a href="#projects" className="btn btn-primary">View My Work</a>
-          <a href="#contact" className="btn btn-secondary">Contact Me</a>
+          <Link to="/projects" className="btn btn-primary">View My Work</Link>
+          <Link to="/contact" className="btn btn-secondary">Contact Me</Link>
         </div>
       </div>
       
